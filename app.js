1086d0f5d863c87e8fad5d1669e510174a51d1be
aayushi vishnoi <aayushivishnoi3@gmail.com>
2022-07-22 11:25:25 +0530

    Port config

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on Heroku // local port 3000");
 });
